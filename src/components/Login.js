6dbd912eb0aa04a70ccfcb0867dc6df9212e1d4b
peterdolan <peterdolannola@gmail.com>
2026-01-30 11:37:20 -0600

    Basic login functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,61 @@
+import { useState } from "react";
+import Input from "./form/Input";
+import { useNavigate, useOutletContext } from "react-router-dom";
+
 const Login = () =>  {
 
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+
+    const { setJwtToken } = useOutletContext();
+    const { setAlertClassName } = useOutletContext();
+    const { setAlertMessage } = useOutletContext();
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (email === "admin@example.com") {
+            setJwtToken("abc");
+            setAlertClassName("d-none");
+            setAlertMessage("");
+            navigate("/");
+        } else {
+            setAlertClassName("alert-danger");
+            setAlertMessage("Invalid Credentials");
+        }
+    }
+
+
     return (
-        <div>
+        <div className="col-md-6 offset-md-3">
             <h2>Login</h2>
             <hr />
+            <form onSubmit={handleSubmit}>
+                <Input 
+                    title="Email Address"
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    autoComplete="email-new"
+                    onChange={(event) => setEmail(event.target.value)}
+                />
+
+                <Input 
+                    title="Password"
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    autoComplete="password-new"
+                    onChange={(event) => setPassword(event.target.value)}
+                />
+
+                <input type="submit" className="btn btn-primary" value="Login"></input>
+                <hr />
+            </form>
         </div>
+        
+        
     )
 }
 
